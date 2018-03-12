85ba1cd55e4b9958f2171a7f3154fd447ece1af8
KelvinSajere <yovwisajere@gmail.com>
2018-03-11 20:48:09 -0600

    Added Comment

--- src/main/java/com/oneChallengePerDay/nineMarch2018/NumberOfCandles.java
+++ src/main/java/com/oneChallengePerDay/nineMarch2018/NumberOfCandles.java
@@ -12,8 +12,9 @@ import java.util.Arrays;
 public class NumberOfCandles {
 	
 	public int noOfCandles(int[] arr) {
-		
+		//get the max value in the array
 		int max=Arrays.stream(arr).max().getAsInt();
+		//filter by max value, count
 		return (int) Arrays.stream(arr).filter(i-> i==max).count();
 		
 		
